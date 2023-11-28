5c74ab10882737af0219f5ad15d8221584c81a39
Nadya <gadiarrr30@gmail.com>
2023-11-28 19:30:35 +0300

    Задача 3 автотестом принята

---
+++ HomeWork001_3/Program.cs
@@ -0,0 +1,11 @@
+﻿// Проверка на четность
+int num1 = new Random().Next(1, 10);
+
+if (num1 % 2 == 0)
+{
+    Console.WriteLine($"Число `{num1}` четное");
+}
+if (num1 % 2 == 1)
+{
+    Console.WriteLine($"Число `{num1}` нечетное");
+}
