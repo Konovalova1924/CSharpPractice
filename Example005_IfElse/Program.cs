06bafa7a66ae2e257c99cd2e7170e6c1f1f4d26d
Nadya <gadiarrr30@gmail.com>
2023-11-28 14:49:03 +0300

    added Example005 IfElse

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿// Если ввели имя Маша то приветсвовать по особенному, иначе приветсвовать как обычно
+Console.WriteLine ($"Введите имя пользователя:");
+string username = Console.ReadLine();
+if (username.ToLower() == "masha")
+{
+    Console.WriteLine("Ура, это же Маша!");
+}
+else 
+{
+    Console.Write("Привет! ");
+    Console.WriteLine(username);
+}
