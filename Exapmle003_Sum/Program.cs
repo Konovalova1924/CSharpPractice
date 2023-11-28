7786450f43189d18cd381ac3db4cbe57dd81cffd
Nadya <gadiarrr30@gmail.com>
2023-11-28 14:32:44 +0300

    add Random

--- Exapmle003_Sum/Program.cs
+++ Exapmle003_Sum/Program.cs
@@ -1,7 +1,10 @@
 ﻿// Создать программу сложения двух чисел
-int numA = 3;
-int numB = 5;
+int numA = new Random().Next(1, 10); // 1 2 3 4 5 6 7 8 9 
+Console.WriteLine(numA);
+int numB = new Random().Next(1, 10); // 1 2 3 4 5 6 7 8 9
+Console.WriteLine(numB); 
 int result = numA + numB;
 Console.WriteLine (result);
+
 
 
