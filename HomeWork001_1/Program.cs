d3b326ff38a370d031c77943cdcda235bc21fa1c
Nadya <gadiarrr30@gmail.com>
2023-11-28 19:04:44 +0300

    Задача 1 автотестом принята

---
+++ HomeWork001_1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Найти большее из двух чисел
+
+int num1 = 3;
+int num2 = 6;
+
+if (num1 > num2)
+{
+    Console.WriteLine ($"Первое число `{num1}` больше чем второе число `{num2}`");
+}
+if (num1 < num2)
+{
+    Console.WriteLine($"Первое число `{num1}` меньше чем второе число `{num2}`");
+}
+if (num1 == num2)
+{
+    Console.WriteLine($"Введенные числа равны `{num1}`");
+}
+
+
