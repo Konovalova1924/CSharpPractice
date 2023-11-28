6cae36dae9c5688c12dea6d0d57958d7ce458503
Nadya <gadiarrr30@gmail.com>
2023-11-28 19:21:28 +0300

    Задача2 автотестом принята

---
+++ HomeWork001_2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Найти максимальное из трех чисел
+int numA = new Random().Next(1,10);
+int numB = new Random().Next(1,10);
+int numC = new Random().Next(1,10);
+Console.WriteLine(numA);
+Console.WriteLine(numB);
+Console.WriteLine(numC);
+int max = numA;
+if (numB > max) 
+{
+    max = numB;
+}
+if (numC > max)
+{
+    max = numC;
+}
+Console.WriteLine($"Max число = {max}");
