1cb2d16f658ccf19e6fac60543c89d4d87f9d349
Nadya <gadiarrr30@gmail.com>
2023-11-28 20:13:42 +0300

    Задача 4 мои табы автотексту не понравились

---
+++ HomeWork001_4/Program.cs
@@ -0,0 +1,20 @@
+﻿// Вывести четные числа интересным способом
+int number = new Random().Next(1, 10);
+int maxcount = 0;
+int i = 2;
+if (number % 2 == 0)
+{
+    maxcount = number;
+} 
+if (number %2 == 1)
+{
+    maxcount = number - 1;
+}
+Console.Write($"number = {number} ->    {i}");
+
+while (i < maxcount)
+{
+    i = i + 2;
+    Console.Write($"    {i}");
+}
+
