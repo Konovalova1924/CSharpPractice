95d979bbe0f227ac072c0e8ef6ca943c379180ed
Nadya <gadiarrr30@gmail.com>
2023-11-29 21:18:43 +0300

    Первая задачка из 2го семинара все разбирали

---
+++ Lesson2_Task1/Program.cs
@@ -0,0 +1,10 @@
+﻿// На вход принимает трехзначное число и удаляет вторую цифру этого числа.
+// 456 -> 46
+
+int num = 851;
+int first = num / 100;
+int second = num % 10;
+int result = first*10 + second;
+Console.WriteLine (result);
+Console.WriteLine ($"{first}\t{second}");
+
