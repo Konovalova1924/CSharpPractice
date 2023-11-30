90583aa1703abffe124d32e7dd9ace1d004fa4ce
Nadya <gadiarrr30@gmail.com>
2023-11-30 15:01:59 +0300

    Определение координатной четверти

---
+++ HomeWork002_2/Program.cs
@@ -0,0 +1,22 @@
+﻿// Определение координатной плоскости
+
+Console.Write("Введите координаты точки Х:");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координаты точки У:");
+int y = Convert.ToInt32(Console.ReadLine());
+if (x > 0 && y > 0)
+{
+    Console.WriteLine($"Координатная четверть №1");
+}
+if (x < 0 && y > 0)
+{
+    Console.WriteLine($"Координатная четверть №2");
+}
+if (x < 0 && y < 0)
+{
+    Console.WriteLine($"Координатная четверть №3");
+}
+if (x > 0 && y < 0)
+{
+    Console.WriteLine($"Координатная четверть №4");
+}
