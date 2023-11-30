b89f983c2b6b17aaf85e38876cbbe40ee2497184
Nadya <gadiarrr30@gmail.com>
2023-11-30 15:37:38 +0300

    В числе из промежутка (10,99) выводит наибольшую цифру

---
+++ HomeWork002_3/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход целое число из отрезка [10, 99] 
+// и показывает наибольшую цифру числа.
+int numA = new Random().Next(10, 100);
+Console.WriteLine($"Случайное число из отрезка 10-99: {numA}");
+int num1 = numA / 10;
+int num2 = numA % 10;
+if (num1 > num2)
+{
+    Console.WriteLine($"Большее {num1}");
+}
+else
+{
+    Console.WriteLine($"Большее {num2}");
+}
