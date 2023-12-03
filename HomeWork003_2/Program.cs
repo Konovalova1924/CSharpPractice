ecff4a48b80e6a218eabec7825d002b254b70349
Nadya <gadiarrr30@gmail.com>
2023-12-04 00:03:33 +0300

    В массиве из случайных 3х-значных чисел подсчитывает кол-во четных чисел

---
+++ HomeWork003_2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задайте массив заполненный случайными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+void EvenQyantity()
+{
+   int [] array = new int[10];
+   Random random = new Random();
+    int count2 = 0;
+    for (int i =0; i<array.Length; i++)
+    {   array[i] = random.Next(100, 1000);
+        Console.Write(array[i] + " ");
+        if (array[i] % 2 == 0)
+        {   count2 = count2 + 1;
+        }
+    }
+Console.WriteLine();
+Console.WriteLine($"кол-во четных чисел в массиве: {count2}");
+}
+
+EvenQyantity();
+
