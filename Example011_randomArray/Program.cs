e33327ceee2329d46ebb18765ab634596b2cce30
Nadya <gadiarrr30@gmail.com>
2023-12-05 12:39:21 +0300

    randomArrayMetod

---
+++ Example011_randomArray/Program.cs
@@ -0,0 +1,46 @@
+﻿void FillArray(int [] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index<length)
+    {
+        collection [index] = new Random().Next(1, 10);
+        index = index + 1;
+    }
+} 
+
+void PrintArra(int [] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position<count)
+    {
+        Console.WriteLine(col[position]);
+        position = position + 1;
+    }
+}
+
+int IndexOf(int [] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+    while (index<count)
+    {
+        if (collection[index]==find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+int [] array1 = new int [10];
+FillArray(array1);
+PrintArra(array1);
+
+Console.WriteLine();
+int pos = IndexOf(array1, 4);
+Console.WriteLine(pos);
