c965897969ae14f45938c572dc2d4c2359aea743
Nadya <gadiarrr30@gmail.com>
2023-12-05 12:19:27 +0300

    arrayfindpos

---
+++ Example010_arrayfindpos/Program.cs
@@ -0,0 +1,15 @@
+﻿// Найти элемент массива, равный find
+
+int [] array = {1, 3, 5, 8, 4, 6, 8, 1};
+int n = array.Length;
+int find = 8;
+int index = 0;
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index = index +1;
+}
